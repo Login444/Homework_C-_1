8d137968d03f989072c7c8f4e56b29007fb6be5d
Artemiy <birukovartemij@gmail.com>
2022-12-17 18:18:56 +0300

    Исправлены ошибки в первом задании

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Введите первое число");
-int num1 = Console.ReadLine();
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int num2 = Console.ReadLine();
-if num1 > num2
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
 {
-    Console.WriteLine($"Число {num1} больше" );
+    Console.WriteLine($"Число {num1} больше");
 }
 else
 {
