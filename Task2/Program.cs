772cc234e7892250a361aa1b6b6d5f8f3453cc5c
Artemiy <birukovartemij@gmail.com>
2022-12-17 18:35:39 +0300

    Написал код для ввода 3 чисел

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.Readline());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше");
+}
+else
+{
+    Console.WriteLine($"Число {num2} больше");
+}
