85398312e2e60239615600f78567c5414a2fd8ac
Artemiy <birukovartemij@gmail.com>
2022-12-17 18:13:00 +0300

    task1 completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,3 +2,11 @@
 int num1 = Console.ReadLine();
 Console.WriteLine("Введите второе число");
 int num2 = Console.ReadLine();
+if num1 > num2
+{
+    Console.WriteLine($"Число {num1} больше" );
+}
+else
+{
+    Console.WriteLine($"Число {num2} больше");
+}
