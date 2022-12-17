b3a7aee9c2167b80a07c8fd42e92ab9c03fa4d60
Artemiy <birukovartemij@gmail.com>
2022-12-17 18:07:21 +0300

    added gitignore file

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+int num2 = Console.ReadLine();
